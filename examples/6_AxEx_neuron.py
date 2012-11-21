820194108b24083d740cc4d1872a68e636abe98d
Eric Jang <ericjang2004@gmail.com>
2012-11-21 16:43:18 -0500

    implemented AdEx real fast. Booya!

---
+++ examples/6_AxEx_neuron.py
@@ -0,0 +1,11 @@
+import os,sys
+parentdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,parentdir)
+
+from pyN import *
+
+single_neuron = AdExPopulation(name='neuron', N=1)
+brain = Network(populations=[single_neuron])
+stim = [{'start':10,'stop':100,'mV':14,'neurons':[0]}]
+results = brain.simulate(experiment_name='Single AdEx Neuron',T=100,dt=0.25,integration_time=30,I_ext={'neuron':stim}, save_data='../data/', properties_to_save=['v','w','psc','I_ext'])
+show_data(results)
